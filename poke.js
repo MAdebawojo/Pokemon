6ca04f568dbaf9ae7c95aa2486cd763465cfac27
MAdebawojo <adebawojomosope@gmail.com>
2024-05-06 08:41:44 +0100

    refactored the code

--- poke.js
+++ poke.js
@@ -1,9 +1,11 @@
 
-for(var i = 1; i <= 206; i++)
+const container = document.querySelector('#myPokes')
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+for(var i = 1; i <= 225; i++)
 {
-    let pokemon = document.createElement("img")
-
-    pokemon.setAttribute('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ i +'.png')
+    const pokemon = document.createElement("img")
+    
+    pokemon.setAttribute('src', `${baseURL}${i}.png`)
 
     document.body.appendChild(pokemon)
     // console.log(pokemon.src)
